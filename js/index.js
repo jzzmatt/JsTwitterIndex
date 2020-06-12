3637f48bd76715c29952c59230f7f753cb44e241
jmateus <jmateus@jmateuss-iMac.local>
2020-06-12 16:28:40 +0100

    JS update

--- js/index.js
+++ js/index.js
@@ -33,6 +33,9 @@ const saveNextPage = (metadata) => {
  * Handle when a user clicks on a trend
  */
 const selectTrend = (e) => {
+    const text = e.innerText;
+    document.getElementById('user-search-input').value = text
+    getTwitterData();
 }
 
 /**
@@ -47,6 +50,7 @@ const nextPageButtonVisibility = (metadata) => {
 const buildTweets = (tweets, nextPage) => {
    let twitterContent = "";
    tweets.map((tweet) => {
+       const createDate = moment(tweet.created_at).fromNow();
        twitterContent += `
         <div class="tweet-container">
             <div class="tweet-user-info">
@@ -57,6 +61,22 @@ const buildTweets = (tweets, nextPage) => {
             </div>
         </div>
        `
+       if(tweet.extended_entities 
+        && tweet.extended_entities.media.length > 0) {
+            twitterContent += buildImages(tweet.extended_entities.media);
+            twitterContent += buildVideo(tweet.extended_entities.media);
+        }
+        twitterContent += `
+            <div class="tweet-text-container">
+                <span class="tweet-text">
+                ${tweet.full_text}
+                </span>
+            </div>
+            <div class="tweet-date>
+              ${createDate}
+            </div>
+            `
+        
    })
    document.querySelector('.tweets-list').innerHTML = twitterContent;
 }
@@ -65,12 +85,45 @@ const buildTweets = (tweets, nextPage) => {
  * Build HTML for Tweets Images
  */
 const buildImages = (mediaList) => {
-
+   let imagesContent = `<div class="tweet-images-container>`;
+   let imageExists = false;
+   mediaList.map((media) => {
+       if (media.type == "photo") {
+           imageExists = true;
+           imagesContent += `<div class="tweet-image" style="background-image: url($(media.media_url_https))">`
+       }
+   });
+   imagesContent += `</div>`;
+   return imageExists ? imagesContent : '';
 }
 
 /**
  * Build HTML for Tweets Video
  */
 const buildVideo = (mediaList) => {
+    let videoContent = `<div class="tweet-video-container">`;
+    let videoExists = false;
+    mediaList.map((media)=>{
+        if(media.type == "video" || media.type == 'animated_gif'){
+            videoExists = true;
+            const video = media.video_info.variants.find((video)=>video.content_type == 'video/mp4');
+            const videoOptions = getVideoOptions(media.type);
+            videoContent += `
+            <video ${videoOptions}>
+                <source src="${video.url}" type="video/mp4">
+                Your browser does not support HTML5 video.
+            </video>
+            `
+        }
+    })
+    videoContent += `</div>`;
+    return (videoExists ? videoContent : '');
+}
 
+const getVideoOptions = (mediaType) => {
+    if (mediaType == 'animated_gif') {
+        return "loop autoplay";
+    } else {
+        return "controls";
+    }
 }
